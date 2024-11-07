8ca15324899f55c53a08a9a707347060eb456b4e
devsys72 <132669084+devsys72@users.noreply.github.com>
2024-11-06 23:47:43 -0300

    Create page-not-found.component.ts

---
+++ src/app/components/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `<h2>404 - Página não encontrada</h2>`,
+  styles: [`h2 { color: red; }`]
+})
+export class PageNotFoundComponent {}
